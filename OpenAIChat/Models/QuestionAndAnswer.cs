6012921b8c5acb629a892300d8f793d26f6907f1
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-22 18:56:47 +0800

    上传项目

---
+++ OpenAIChat/Models/QuestionAndAnswer.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenAIChat.Models;
+
+public class QuestionAndAnswer
+{
+    public string Question { get; set; }
+    public List<string> Anwsers { get; set; }
+
+    public QuestionAndAnswer(string question, List<string> answers)
+    {
+        Question = question;
+        Anwsers = answers;
+    }
+}
